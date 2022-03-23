6db0870c13ad2f2f8e3b5afee203e97b1d842b89
ParatrooperAndy <1468492471@qq.com>
2022-03-23 12:33:16 +0800

    update some structure

---
+++ src/main/java/model/User.java
@@ -0,0 +1,42 @@
+package model;
+
+// 用户类
+public class User {
+
+    private int id; // 用户编号
+    private String username; // 用户名
+    private String password; // 密码
+    private String email; // 电子邮箱
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
