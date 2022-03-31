546ab8d8cd8c1742393b34cf48f05b9146adafea
ParatrooperAndy <1468492471@qq.com>
2022-03-31 16:39:06 +0800

    update todos for manage

--- src/main/java/view/historyui.java
+++ src/main/java/view/historyui.java
@@ -9,6 +9,8 @@ import java.awt.Dimension;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class HistoryUI {
     // 这里使用swing：历史记录和用户管理界面；后续可转为Java Web
@@ -47,7 +49,7 @@ public class HistoryUI {
         // 菜单棒
         jf.setJMenuBar(menubar);
         // 菜单项
-        final JMenuItem menu1 = new JMenuItem("用户管理");
+        final JMenuItem menu1 = new JMenuItem("用户管理:" + MapTestTool.loginUserName);
         final JMenuItem menu2 = new JMenuItem("用户操作记录");
         final JMenuItem menu3 = new JMenuItem("记录模糊查找");
         menubar.add(menu1);
@@ -55,17 +57,18 @@ public class HistoryUI {
         menubar.add(menu3);
 
         // 定义二维数组作为表格数据
-        Object[][] tableData = {
-                new Object[] { "李清照", 29, "女" },
-                new Object[] { "苏格拉底", 56, "男" },
-                new Object[] { "李白", 35, "男" },
-                new Object[] { "弄玉", 18, "女" },
-                new Object[] { "虎头", 2, "男" }
-        };
+        Object[][] tableData = {};
         // 定义一维数据作为列标题
-        Object[] columnTitle = { "姓名", "年龄", "性别" };
-        JTable hisTable = new JTable(tableData, columnTitle);
+        Object[] columnTitle = { "用户名", "地图名称", "错误位置", "错误类型", "错误描述", "上报序号" };
+        JTable hisTable = new JTable();
+        hisTable.setModel(new DefaultTableModel(tableData, columnTitle));
         p.add(new JScrollPane(hisTable));
+
+        // 设置一下居中对齐
+        DefaultTableCellRenderer cr = new DefaultTableCellRenderer();
+        cr.setHorizontalAlignment(JLabel.CENTER);
+        hisTable.setDefaultRenderer(Object.class, cr);
+        hisTable.setEnabled(false); // 不可随意编辑
 
         // 设置表格内容颜色
         hisTable.setForeground(Color.BLACK); // 字体颜色
@@ -85,7 +88,76 @@ public class HistoryUI {
         // 第一列列宽设置为40
         hisTable.getColumnModel().getColumn(0).setPreferredWidth(40);
         // 设置滚动面板视口大小（超过该大小的行数据，需要拖动滚动条才能看到）
-        hisTable.setPreferredScrollableViewportSize(new Dimension(400, 300));
+        hisTable.setPreferredScrollableViewportSize(new Dimension(600, 300));
+
+        // 用户管理
+        menu1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFrame fr6 = new JFrame();
+                fr6.setTitle("用户信息管理");
+                fr6.setSize(400, 330);
+                fr6.setLocationRelativeTo(null);
+                fr6.setResizable(false);
+                fr6.setVisible(true);
+                fr6.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                fr6.setIconImage(im);
+                JLabel imgLabel = new JLabel(img); // 将背景图放在标签里。
+                fr6.getLayeredPane().add(imgLabel, Integer.valueOf(Integer.MIN_VALUE));
+                imgLabel.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+                imgLabel.setOpaque(true);
+
+                JPanel p6 = new JPanel();
+                fr6.setContentPane(p6); // 面板
+                p6.setOpaque(false);
+
+                JTextArea textArea6 = new JTextArea();
+                textArea6.setLineWrap(true);
+                textArea6.setSize(250, 220);
+                p6.add("Center", textArea6);
+                textArea6.setOpaque(false);
+                textArea6.setForeground(Color.RED);
+                textArea6.setFont(new Font("仿宋", Font.BOLD, 18));
+
+                textArea6.setEditable(false);
+                textArea6.append("用户信息管理\n\n"
+                        + "用户名: \n"
+                        + "密码: \n"
+                        + "id: \n");
+                textArea6.setEditable(false);
+
+            }
+        });
+
+        // 用户名错误上报信息
+        menu2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (MapTestTool.loginFlag == 1) {
+                    // 在数据库中通过用户名搜索用户操作记录，这里注意join的用法，用户管理界面可以渲染更漂亮一些
+                    ArrayList<String> his4dbString = MapTestTool.searchHistoryfromDBbyName(MapTestTool.loginUserName);
+                    String searchHistorysString = "用户 " + MapTestTool.loginUserName + " 的远端数据库相关记录:\n\n";
+
+                    for (int i = 0; i < his4dbString.size(); i++) {
+                        System.out.println(his4dbString.get(i));
+                        // 字符串格式优化一下
+                        String tempHistorysString = String.join("\n",
+                                "用户记录编号:" + his4dbString.get(i).split("&")[0],
+                                "操作地图名称:" + his4dbString.get(i).split("&")[1],
+                                "上报错误位置:" + his4dbString.get(i).split("&")[2],
+                                "上报错误类型:" + his4dbString.get(i).split("&")[3],
+                                "上报错误描述:" + his4dbString.get(i).split("&")[4] + "\n\n");
+
+                        System.out.println(tempHistorysString);
+                        searchHistorysString += tempHistorysString;
+                    }
+                    JOptionPane.showMessageDialog(null, searchHistorysString, "用户远端数据库相关记录",
+                            JOptionPane.PLAIN_MESSAGE, imageIcon_menu);
+                } else {
+                    JOptionPane.showMessageDialog(null, "请您先登陆账号，再查看用户操作记录！\n或者您可以直接搜索查看!", "未登录",
+                            JOptionPane.PLAIN_MESSAGE,
+                            imageIcon_menu);
+                }
+            }
+        });
 
         // 模糊搜索查找
         menu3.addActionListener(new ActionListener() {
@@ -110,6 +182,8 @@ public class HistoryUI {
                                 "上报错误类型:" + his4dbkeywordString.get(i).split("&")[3],
                                 "上报错误描述:" + his4dbkeywordString.get(i).split("&")[4] + "\n\n");
 
+                        // tableData.
+
                         System.out.println(tempHistorysString);
                         searchHistoryString += tempHistorysString;
                     }
@@ -124,7 +198,4 @@ public class HistoryUI {
 
     }
 
-    public static void main(String[] args) {
-        new HistoryUI();
-    }
 }
